23eb78b3f3cd51536ee2315a828d299272212bd6
sivepanda <68242448+sivepanda@users.noreply.github.com>
2021-06-12 23:27:06 -0500

    base

--- js/productpicker.js
+++ js/productpicker.js
@@ -1,1 +1,14 @@
-//blah
+var image = document.getElementById("carousel");
+var pickerButtons = document.getElementsByClassName("dot");
+image.src = "resources/100/100_0.png";
+
+var pickerButtonsArr = Array.from(pickerButtons);
+
+pickerButtonsArr.forEach(addPickerListener);
+
+function addPickerListener(item, index) {
+    document.getElementById(index.toString()).src = "resources/100/100_" + index + ".png";
+    item.addEventListener("click", function() {
+        image.src = "resources/100/100_" + index + ".png";
+    });
+}
