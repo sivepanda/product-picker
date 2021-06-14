9966a37ee34818c8d83fddfb05479b042287bda4
sivepanda <68242448+sivepanda@users.noreply.github.com>
2021-06-13 21:48:23 -0500

    cleanup, functionality increase

--- js/productpicker.js
+++ js/productpicker.js
@@ -1,14 +1,25 @@
 var image = document.getElementById("carousel");
 var pickerButtons = document.getElementsByClassName("dot");
-image.src = "resources/100/100_0.png";
 
 var pickerButtonsArr = Array.from(pickerButtons);
 
 pickerButtonsArr.forEach(addPickerListener);
 
 function addPickerListener(item, index) {
-    document.getElementById(index.toString()).src = "resources/100/100_" + index + ".png";
+    var parentDiv = item.parentNode;
+    var id = parentDiv.getAttribute("id");
+    image.src = "resources/" + id + "/colors/" + id + "_0.png";
+    document.getElementById(index.toString()).src = "resources/" + id + "/colors/" + id + "_" + index + ".png";
+
     item.addEventListener("click", function() {
-        image.src = "resources/100/100_" + index + ".png";
+        image.src = "resources/" + id + "/products/" + id + "_" + index + ".png";
+        var span = document.getElementById("(" + index.toString() + ")");
+        pickerButtonsArr.forEach(reset);
+        span.setAttribute("style", "background-color:black");
     });
+}
+
+function reset(item, index) {
+    var span = document.getElementById("(" + index.toString() + ")");
+    span.setAttribute("style", "background-color:grey");
 }
